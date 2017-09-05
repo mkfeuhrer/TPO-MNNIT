55b25fd98514da661710bda2b967d5b92f698f94
nvrocks <neerajverma.mnnit@gmail.com>
2017-09-05 23:13:18 +0530

    added notification service

---
+++ app/src/main/java/com/example/mohit/tpomnnit/notification/NotificationHandler.java
@@ -0,0 +1,42 @@
+package com.example.mohit.tpomnnit.notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import android.support.v4.app.NotificationCompat;
+
+import com.example.mohit.tpomnnit.Companies;
+import com.example.mohit.tpomnnit.CompanyStudent;
+
+/**
+ * Created by neera on 9/5/2017.
+ */
+
+public class NotificationHandler {
+    public static void showNotification(Context context, int icon, String title, String text) {
+        NotificationCompat.Builder nBuilder
+                = new NotificationCompat.Builder(context)
+                .setSmallIcon(icon)
+                .setContentTitle(title)
+                .setContentText(text);
+        Intent intent = new Intent(context, CompanyStudent.class);
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        taskStackBuilder.addParentStack(CompanyStudent.class);
+        taskStackBuilder.addNextIntent(intent);
+        PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        nBuilder.setContentIntent(pendingIntent);
+        nBuilder.setAutoCancel(true);
+        nBuilder.setLights(Color.BLUE, 500, 500);
+        nBuilder.setStyle(new NotificationCompat.InboxStyle());
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        nBuilder.setSound(alarmSound);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, nBuilder.build());
+    }
+}
---
+++ app/src/main/java/com/example/mohit/tpomnnit/services/NotificationService.java
@@ -0,0 +1,75 @@
+package com.example.mohit.tpomnnit.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.mohit.tpomnnit.Companies;
+import com.example.mohit.tpomnnit.R;
+import com.example.mohit.tpomnnit.notification.NotificationHandler;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by neera on 9/5/2017.
+ */
+
+public class NotificationService extends IntentService{
+
+    DatabaseReference mDatabase;
+    String companyId;
+    int n,i=-10;
+    public NotificationService() {
+        super("Notification Service");
+        Log.i("service", "Constructor called");
+        //System.out.println("Service Constructor called");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.i("service", "In onHandleIntent");
+        mDatabase= FirebaseDatabase.getInstance().getReference("companies");
+        companyId=mDatabase.push().getKey();
+        //System.out.println("in onHandler called");
+        while(true)
+        {
+            Log.i("service", "In while");
+
+            ValueEventListener vel = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    n= (int) dataSnapshot.getChildrenCount();
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+            mDatabase.addValueEventListener(vel);
+            if(i==-10)
+            {
+                i=n;
+                continue;
+            }
+            if(i!=n)
+            {
+                //Log.i("service", "In while loop");
+                i=n;
+                System.out.println("new company added");
+                NotificationHandler.showNotification(this, R.drawable.company4, "Company added","a new company is added click here to check");
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
