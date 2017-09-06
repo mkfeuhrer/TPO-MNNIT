4f6904a5ac0548bccc9b612c8a716209b054c53a
Mohit Khare <mohitfeuhrer@gmail.com>
2017-09-07 03:09:00 +0530

    Updating Landing page UI

--- app/src/main/java/com/example/mohit/tpomnnit/Landing.java
+++ app/src/main/java/com/example/mohit/tpomnnit/Landing.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.mohit.tpomnnit.services.NotificationService;
@@ -13,12 +14,17 @@ import com.example.mohit.tpomnnit.services.NotificationService;
 public class Landing extends AppCompatActivity {
 
     private Button tpoadmin,student;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
         Intent serviceIntent =new Intent(Landing.this, NotificationService.class);
         startService(serviceIntent);
+
+        imageView = (ImageView) findViewById(R.id.landingimage);
+
+        imageView.setImageResource(R.drawable.mnnit);
 
         tpoadmin = (Button)findViewById(R.id.tpoadmin);
         student  = (Button)findViewById(R.id.student);
